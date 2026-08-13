037d14563e7f971e31fa1586fbc4670951eddd1e
diana-michaud <michaud.diana.hm@gmail.com>
2026-08-12 20:29:10 -0600

    Escribí el siguiente texto en 3 lineas de código: "Hola, Diana!".

--- TRABAJO_01_FDP/Program.cs
+++ TRABAJO_01_FDP/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+// ACTIVIDAD 1 - 3 líneas de código
+
+Console.Write("Hola");
+Console.Write(", ");
+Console.Write("Diana!");

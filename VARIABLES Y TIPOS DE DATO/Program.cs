ef335944f2aeeb1ddf701613500cbff6c2455fd3
diana-michaud <michaud.diana.hm@gmail.com>
2026-08-19 09:42:26 -0600

    Anoté un ejemplo para futuras actividades sobre variables y tipos de dato

--- VARIABLES Y TIPOS DE DATO/Program.cs
+++ VARIABLES Y TIPOS DE DATO/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// EJEMPLO: Obtener y mostrar los valores mínimos y máximos de un tipo de dato
+sbyte minValueSbyte = sbyte.MinValue;
+sbyte maxValueSbyte = sbyte.MaxValue;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("SByte:");
+Console.WriteLine("MinValueSbyte: " + minValueSbyte);
+Console.WriteLine("MaxValueSbyte: " + maxValueSbyte);
+
+// Símbolo de la suma? +
+// Símbolo de la resta -
+// Símbolo de la multiplicación *
+// Símbolo de la división /

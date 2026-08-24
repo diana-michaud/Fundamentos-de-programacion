a8b30bd0a92bba8e4243c5cf8dfad3b6fc6c2872
diana-michaud <michaud.diana.hm@gmail.com>
2026-08-24 09:35:40 -0600

    Apunte de clase: Variables de tipo explicito, implicito y constantes.

---
+++ TIPOS IMPLICITOS Y EXPLICITOS/Program.cs
@@ -0,0 +1,14 @@
+﻿// Variables de tipo explicito
+sbyte sbyteVal = 1;
+Console.WriteLine("Tipo de sbyteVal: " + sbyteVal.GetType());
+
+// Variables de tipo implicito
+var num1 = 1;
+Console.WriteLine("Tipo de num1: " + num1.GetType());
+
+var num2 = 1_000_000_000_000_000_000;
+Console.WriteLine("Tipo de num2: " + num2.GetType());
+
+// Constantes
+const sbyte MIN_VALUE_SBYTE = -128;
+Console.WriteLine("Tipo de MIN_VALUE_sbyte: " + MIN_VALUE_SBYTE.GetType());

76a73df6cc67d39499fc73e675a513cf65f070cb
Diana Michaud <michaud.diana.hm@gmail.com>
2026-08-31 10:03:56 -0600

    Nombre, HP y apodo del jugdor

---
+++ TRABAJO_04_FDP/Program.cs
@@ -0,0 +1,8 @@
+﻿// Generar un programa que declare un entero e imprima la vida del jugador.
+// Pedir el nombre del jugador.
+// Pedir el apodo del jugador e imprimirlo entre comillas dobles.
+
+Console.Write("Yoda \"La rana\" \n");
+Console.Write("HP: 150\n");
+Console.Write("Apodo: ");
+string apodo = Console.ReadLine();
